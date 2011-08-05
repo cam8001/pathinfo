bda1d51b7d9af41396fe0920dafddb3e989e31d7
Cameron Tod <cameron.tod@gmail.com>
2011-08-05 03:11:16 +0100

    Added more features to pathinfo_detail(), adding theme implemenation.
    I'll need to work out an extensible theme implementation to add extra
    info blocks to the detail block.

---
+++ infopane.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Default template for pathinfo.
+ * 
+ * Available variables:
+ * 
+ */
+dpm($infopane);
+dpm($variables['infopane']);
+
+
+?>
+<div id="pathinfo-infopane">
+  <h1>Path</h1> 
+</div>
