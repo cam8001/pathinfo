1a62b061ae8ea4290582642d8c8330bd05e52675
Cameron Tod <cameron.tod@gmail.com>
2011-08-21 00:29:50 +0100

    Added AHAH form submision

--- infopane.tpl.php
+++ infopane.tpl.php
@@ -5,13 +5,23 @@
  * Available variables:
  * 
  */
-dpm($pathinfo, 'theme_function');
+dpm($variables, 'theme function variables');
 dpm(get_defined_vars(), 'theme_function');
 
-dpm($variables['pathinfo'], 'theme_function');
+//dpm($variables['pathinfo'], 'theme_function');
+
+foreach($variables['pathinfo'] as $key=>$value) {
+  if ($key == 'module_info') {
+    //foreach ($value as $key=>$va)
+  } else {
+    $items[] = t('%label: @value', array('%label' => $key, '@value' => $value));
+  }
+}
 
 
 ?>
 <div id="pathinfo-infopane">
-  <h1>Path</h1> 
+  <?php
+    echo theme('item_list', $items);
+  ?>
 </div>
--- pathinfo.js
+++ pathinfo.js
@@ -1,7 +1,15 @@
 Drupal.behaviors.pathinfo = function(context) {
   // Catch the submit action of the main form (at admin/settings/pathinfo)
   $('form#pathinfo-lookup-form', context).submit(function(){
-    return true;
+    var path = $('input[name=path]', this).val();
+    var url = Drupal.settings.basePath + 'admin/settings/pathinfo/ajax/' + path;
+    $.getJSON(url, function(result){
+      $('#pathinfo-result').html(result.data);
+      console.log(result);
+    });
+    
+    //console.log(this);
+    return false;
   });
 }
 
